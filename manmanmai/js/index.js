b67a60f53030169264acbb69549cb313c10704b5
lianganan <894795786@qq.com>
2018-10-12 15:43:25 +0800

    慢慢买--首页渲染完成

---
+++ manmanmai/js/index.js
@@ -0,0 +1,32 @@
+$(function () {
+  function nav_render() {
+  //导航渲染
+    $.ajax({
+      type:'get',
+      url:'http://127.0.0.1:9090/api/getindexmenu',
+      dataType:'json',
+      success:function (info) {
+        console.log(info);
+        $('.mmm_nav ul').html(template('tmp1',info));
+      }
+    })
+  };
+
+  function products_render(){
+  //  商品列表渲染
+    $.ajax({
+      type: 'get',
+      url:'http://127.0.0.1:9090/api/getmoneyctrl',
+      dataType: 'json',
+      success:function (info) {
+        console.log(info);
+        $('.products ul').html(template('tmp2',info));
+      }
+    })
+  }
+
+  nav_render();
+  products_render();
+
+  
+})
